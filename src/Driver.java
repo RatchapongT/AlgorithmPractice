20ab6d222a66b14011dd9e0fbb5fd9ba1123543b
RatchapongT <ratchapong.t@gmail.com>
2016-04-05 19:59:54 -0400

    Remove Driver

--- src/Driver.java
+++ src/Driver.java
@@ -13,45 +13,5 @@ import java.util.Set;
  */
 public class Driver {
     public static void main(String[] args) {
-        String[] test = {"so", "what", "whatsoever", "son", "sales", "per", "person", "chat", "ever", "snapchat", "snap", "salesperson"};
-        String[] answer = simpleWords(test);
-        System.out.println(simpleWords(test));
-    }
-
-    public static String[] simpleWords(String[] words) {
-        Set<String> set = new HashSet<>(Arrays.asList(words));
-        Set<String> answer = new HashSet<>();
-        for (String word: set) {
-            if(!simpleWordsHelper(word, set)) {
-                answer.add(word);
-            }
-        }
-        return answer.toArray(new String[answer.size()]);
-
-
-
-    }
-
-    public static boolean simpleWordsHelper(String s, Set<String> dict) {
-        int minLen = Integer.MAX_VALUE;
-        int maxLen = Integer.MIN_VALUE;
-        for (String word: dict){
-            minLen = Math.min(minLen, word.length());
-            maxLen = Math.max(maxLen, word.length());
-        }
-        boolean[] found = new boolean[s.length() + 1];
-        found[0] = true;
-        for(int i = minLen; i<= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                if ((i - j) < maxLen || !found[j]) continue;
-                String substring = s.substring(j, i);
-                boolean same = substring.equals(s);
-                if (dict.contains(substring) && !same) {
-                    found[i] = true;
-                    break;
-                }
-            }
-        }
-        return found[s.length()];
     }
 }
