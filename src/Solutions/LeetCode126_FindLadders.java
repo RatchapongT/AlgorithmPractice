f54aa56a6a5a40eea7c8862ebe8417b60ca80424
RatchapongT <ratchapong.t@gmail.com>
2016-04-10 21:49:42 -0400

    remove check ladders

--- src/Solutions/LeetCode126_FindLadders.java
+++ src/Solutions/LeetCode126_FindLadders.java
@@ -26,7 +26,6 @@ public class LeetCode126_FindLadders {
         List<List<String>> resultList = new ArrayList<>();
         while (thisLevel.size() != 0) {
             Map<String, List<List<String>>> nextLevel = new HashMap<>();
-            Set<String> visited = new HashSet<String>();
             Map<String, List<List<String>>> temp;
 
             if (lastLevel.size() < thisLevel.size()) {
@@ -34,7 +33,7 @@ public class LeetCode126_FindLadders {
                 thisLevel = lastLevel;
                 lastLevel = temp;
             }
-            boolean getResults = false;
+            boolean found = false;
             for (String key : thisLevel.keySet()) {
                 char[] charArray = key.toCharArray();
                 for (int i = 0; i < key.length(); i++) {
@@ -45,34 +44,38 @@ public class LeetCode126_FindLadders {
                         if (lastLevel.containsKey(queryWord)) {
                             List<List<String>> list1 = thisLevel.get(key);
                             List<List<String>> list2 = lastLevel.get(queryWord);
+                            List<List<String>> tempList;
+                            if (list1.get(0).get(0).equals(endWord)) {
+                                tempList = list1;
+                                list1 = list2;
+                                list2 = tempList;
+                            }
                             for (int j = 0; j < list1.size(); j++) {
                                 for (int k = 0; k < list2.size(); k++) {
                                     List<String> result = new ArrayList<String>(list1.get(j));
                                     for (int l = list2.get(k).size() - 1; l >= 0; l--) {
                                         result.add(list2.get(k).get(l));
                                     }
-                                    if (result.get(0) != beginWord) {
-                                        Collections.reverse(result);
-                                    }
                                     resultList.add(result);
-
                                 }
                             }
-                            getResults = true;
-                        } else if (wordList.contains(queryWord) && !getResults) {
-                            if (!visited.contains(queryWord)) visited.add(queryWord);
-                            if (!nextLevel.containsKey(queryWord)) nextLevel.put(queryWord, new ArrayList<>());
+                            found = true;
+                        } else if (!found && wordList.contains(queryWord)) {
+                            if (!nextLevel.containsKey(queryWord)) {
+                                nextLevel.put(queryWord, new ArrayList<>());
+                            }
                             for (List<String> list : thisLevel.get(key)) {
-                                nextLevel.get(queryWord).add(new ArrayList<>(list));
-                                nextLevel.get(queryWord).get(nextLevel.get(queryWord).size() - 1).add(queryWord);
+                                List<String> newList = new LinkedList<>(list);
+                                newList.add(queryWord);
+                                nextLevel.get(queryWord).add(newList);
                             }
                         }
                     }
                     charArray[i] = original;
                 }
             }
-            if (getResults) break;
-            wordList.removeAll(visited);
+            if (found) break;
+            wordList.removeAll(nextLevel.keySet());
             thisLevel = nextLevel;
         }
         return resultList;
